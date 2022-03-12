b1fc955d9d338fa31cc7657ea05e8e7cf0f8c53c
Fhasghrge <1969533391@qq.com>
2022-03-12 15:02:05 +0800

    init

---
+++ server/src/index.js
@@ -0,0 +1,19 @@
+const Koa = require('koa');
+const bodyParser = require('koa-bodyparser')();
+const staticCache = require('koa-static-cache');
+const path = require('path');
+const logger = require('koa-logger')
+
+const router = require('./routers')
+
+const app = new Koa();
+
+app.use(logger())
+app.use(bodyParser);
+app.use(staticCache(path.resolve(__dirname, './public')))
+
+app.use(router.routes());
+
+app.listen(4000, () => {
+  console.log('server is listening');
+})
